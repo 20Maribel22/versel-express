990766144a2dd17768eeca4bbffbe29e52b33e28
Marina Belan <belanm77@gmail.com>
2024-02-26 13:54:37 +0600

    new

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
 app.get('/',(req,res) => {
     res.send("Express versel app response")
 })
 
-app.listen(5000,() => {
-console.log('app is running')
+app.listen(PORT,() => {
+console.log('Server is running on port'+ PORT)
 })
